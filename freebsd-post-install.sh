320e3b089a5cf1ce665fda39fce85d3b9449c81e
Demir Kolak <112659238+demirkolak0@users.noreply.github.com>
2025-05-12 19:43:32 +0300

    v0.0.2

--- freebsd-post-install.sh
+++ freebsd-post-install.sh
@@ -5,7 +5,7 @@ b=""
 de=""
 kboardlang=""
 read -p "Please enter your username: " uname
-pkg install xorg vim neovim emacs libreoffice
+pkg install xorg vim neovim emacs libreoffice sudo
 echo 'snd_driver_load="YES"' >> /etc/rc.conf
 beep
 echo 'sysrc linux_enable="YES"' >> /etc/rc.conf
@@ -14,7 +14,7 @@ read -p "You should set up userland for Linux yourself. Enter to continue." b
 pw groupmod video -m $uname
 pw groupmod video -m root
 read -p "You should set up graphic drivers yourself. Enter to continue." a
-read -p "Please select your Desktop Environment(XFCE, KDE, GNOME, MATE, Cinnamon, LXQT)"
+read -p "Please select your Desktop Environment(XFCE, KDE, GNOME, MATE, Cinnamon, LXQT): " de
 if [ $de == "XFCE" ]; then
 	pkg install xfce lightdm lightdm-gtk-greeter firefox
 	echo "proc                    /proc           procfs  rw              0       0" >> /etc/fstab
@@ -24,7 +24,7 @@ elif [ $de == "KDE" ]; then
 	pkg install kde sddm firefox
 	echo 'sysrc dbus_enable="YES"' >> /etc/rc.conf
 	echo 'sysrc sddm_enable="YES"' >> /etc/rc.conf
-	read -p "Your default keyboard language will be english. If you want to change it please enter the language. For exp. for english please enter en_US" $kboardlang
+	read -p "Your default keyboard language will be english. If you want to change it please enter the language. (For exp. for english please enter en_US): " $kboardlang
 	sysrc sddm_lang="$kboardlang"
 elif [ $de == "GNOME" ]; then
 	pkg install gnome firefox
@@ -46,7 +46,7 @@ elif [ $de == "LXQT" ]; then
 	echo "proc                    /proc           procfs  rw              0       0" >> /etc/fstab
         echo 'sysrc dbus_enable="YES"' >> /etc/rc.conf
 	echo 'sysrc sddm_enable="YES"' >> /etc/rc.conf
-	read -p "Your default keyboard language will be english. If you want to change it please enter the language. For exp. for english please enter en_US" $kboardlang
+	read -p "Your default keyboard language will be english. If you want to change it please enter the language. (For exp. for english please enter en_US.) :" $kboardlang
         sysrc sddm_lang="$kboardlang"
 else
 	echo "You entered a wrong name. There will be no Desktop Environments."
