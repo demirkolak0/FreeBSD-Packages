d2093f8fd81ac53920f82850f4ac4da41b267553
Demir Kolak <112659238+demirkolak0@users.noreply.github.com>
2025-05-12 21:35:50 +0300

    v0.0.3
    Bug Fix

--- freebsd-post-install.sh
+++ freebsd-post-install.sh
@@ -4,7 +4,8 @@ a=""
 de=""
 kboardlang=""
 read -p "Please enter your username: " uname
-pkg install xorg vim neovim emacs libreoffice sudo neofetch
+pkg install -y vim neovim emacs libreoffice sudo neofetch
+pkg install -y xorg
 echo 'snd_driver_load="YES"' >> /etc/rc.conf
 beep
 pw groupmod video -m $uname
@@ -12,33 +13,33 @@ pw groupmod video -m root
 read -p "You should set up graphic drivers yourself. Enter to continue." a
 read -p "Please select your Desktop Environment(XFCE, KDE, GNOME, MATE, Cinnamon, LXQT): " de
 if [ $de == "XFCE" ]; then
-	pkg install xfce lightdm lightdm-gtk-greeter firefox
+	pkg install -y xfce lightdm lightdm-gtk-greeter firefox
 	echo "proc                    /proc           procfs  rw              0       0" >> /etc/fstab
 	sysrc dbus_enable="YES"
 	sysrc lightdm_enable="YES"
 elif [ $de == "KDE" ]; then
-	pkg install kde sddm firefox
+	pkg install -y kde sddm firefox
 	sysrc dbus_enable="YES"
 	sysrc sddm_enable="YES"
 	read -p "Your default keyboard language will be english. If you want to change it please enter the language. (For exp. for english please enter en_US): " $kboardlang
 	sysrc sddm_lang="$kboardlang"
 elif [ $de == "GNOME" ]; then
-	pkg install gnome firefox
+	pkg install -y gnome firefox
 	echo "proc                    /proc           procfs  rw              0       0" >> /etc/fstab
 	sysrc dbus_enable="YES"
         sysrc gdm_enable="YES"
 elif [ $de == "MATE" ]; then
-	pkg install mate lightdm lightdm-gtk-greeter firefox
+	pkg install -y mate lightdm lightdm-gtk-greeter firefox
 	echo "proc                    /proc           procfs  rw              0       0" >> /etc/fstab
         sysrc dbus_enable="YES"
 	sysrc lightdm_enable="YES"
 elif [ $de == "Cinnamon" ]; then
-	pkg install cinnamon lightdm lightdm-gtk-greeter firefox
+	pkg install -y cinnamon lightdm lightdm-gtk-greeter firefox
 	echo "proc                    /proc           procfs  rw              0       0" >> /etc/fstab
         sysrc dbus_enable="YES"
         sysrc lightdm_enable="YES"
 elif [ $de == "LXQT" ]; then
-	pkg install lxqt sddm firefox
+	pkg install -y lxqt sddm firefox
 	echo "proc                    /proc           procfs  rw              0       0" >> /etc/fstab
         sysrc dbus_enable="YES"
 	sysrc sddm_enable="YES"
