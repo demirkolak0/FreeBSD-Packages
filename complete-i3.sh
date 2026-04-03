8e1912d73ca5921ae65a6c14d57d3234c1a5176a
Demir Kolak <demirkolak8@gmail.com>
2026-04-03 12:26:54 +0300

    Update complete-i3.sh

--- complete-i3.sh
+++ complete-i3.sh
@@ -2,7 +2,8 @@
 sysrc linux_enable="YES"
 service linux start
 pkg install -y vim git libreoffice doas fastfetch xorg firefox wifibox e2fsprogs xfsprogs exfat-utils fusefs-exfat fusefs-ntfs fusefs-hfsfuse fusefs-lkl fusefs-simple-mtpfs linux_base-rl9
-pkg install -y i3 i3status i3lock-color feh dmenu alacritty debootstrap thunar xarchiver thunar-archive-plugin unrar rar zip unzip 7-zip xfce4-clipman-plugin networkmgr
+pkg install -y i3 i3status i3lock-color feh dmenu alacritty debootstrap thunar xarchiver thunar-archive-plugin unrar rar zip unzip 7-zip xfce4-clipman-plugin networkmgr xdg-user-dirs
+xdg-user-dirs-update
 pw gropmod video -m root
 cp ./rc.conf /etc/rc.conf
 cp ./loader.conf /boot/loader.conf
