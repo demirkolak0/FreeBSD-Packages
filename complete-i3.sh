f2fcb82dcc24d3afaeae92fc274434c190cc04f1
Demir Kolak <demirkolak8@gmail.com>
2026-04-03 12:22:40 +0300

    Update complete-i3.sh

--- complete-i3.sh
+++ complete-i3.sh
@@ -2,8 +2,8 @@
 sysrc linux_enable="YES"
 service linux start
 pkg install -y vim git libreoffice doas fastfetch xorg firefox wifibox e2fsprogs xfsprogs exfat-utils fusefs-exfat fusefs-ntfs fusefs-hfsfuse fusefs-lkl fusefs-simple-mtpfs linux_base-rl9
-pkg install -y i3 i3status i3lock-color feh dmenu alacritty debootstrap
-pw groupmod video -m root
+pkg install -y i3 i3status i3lock-color feh dmenu alacritty debootstrap thunar xarchiver thunar-archive-plugin unrar rar zip unzip 7-zip xfce4-clipman-plugin networkmgr
+pw gropmod video -m root
 cp ./rc.conf /etc/rc.conf
 cp ./loader.conf /boot/loader.conf
 cp ./bhyve.conf /usr/local/etc/wifibox/bhyve.conf
@@ -14,6 +14,8 @@ cp -R ./i3 /home/demir/.config/
 cp -R ./i3status /home/demir/.config/
 cp .Xresources /home/demir/.Xresources
 cp .xinitrc /home/demir/.xinitrc
+mkdir -P /home/demir/Pictures/Screenshots
+cp -R ./Wallpapers /home/demir/Pictures/
 mkdir -P /compat/debian/
 debootstrap trixie /compat/debian http://deb.debian.org/debian/
 chown -R demir:demir /home/demir
