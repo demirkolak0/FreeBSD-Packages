835219ba6301b892898eac22a56c2f30a456e455
Demir Kolak <demirkolak8@gmail.com>
2026-04-03 12:14:03 +0300

    Add files via upload

---
+++ complete-i3.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sysrc linux_enable="YES"
+service linux start
+pkg install -y vim git libreoffice doas fastfetch xorg firefox wifibox e2fsprogs xfsprogs exfat-utils fusefs-exfat fusefs-ntfs fusefs-hfsfuse fusefs-lkl fusefs-simple-mtpfs linux_base-rl9
+pkg install -y i3 i3status i3lock-color feh dmenu alacritty debootstrap
+pw groupmod video -m root
+cp ./rc.conf /etc/rc.conf
+cp ./loader.conf /boot/loader.conf
+cp ./bhyve.conf /usr/local/etc/wifibox/bhyve.conf
+cp ./fstab.dk /etc/fstab
+mkdir -p /home/demir/.config/
+cp -R ./alacritty /home/demir/.config/
+cp -R ./i3 /home/demir/.config/
+cp -R ./i3status /home/demir/.config/
+cp .Xresources /home/demir/.Xresources
+cp .xinitrc /home/demir/.xinitrc
+mkdir -P /compat/debian/
+debootstrap trixie /compat/debian http://deb.debian.org/debian/
+chown -R demir:demir /home/demir
+pkg install -y foreign-cdm chromium
+git clone --depth 1 https://git.FreeBSD.org/ports.git /usr/ports
+cd /usr/ports/www/linux-widevine-cdm
+make install clean
