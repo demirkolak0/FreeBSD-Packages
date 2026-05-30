b3700ce10ec310fb23e6256e98c940f0c81a1d0b
Demir Kolak <demirkolak8@gmail.com>
2026-05-30 16:27:15 +0300

    Update only-kde.sh

--- only-kde.sh
+++ only-kde.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 sysrc linux_enable="YES"
 service linux start
-pkg install -y vim zsh libreoffice doas fastfetch xorg firefox wifibox e2fsprogs xfsprogs exfat-utils fusefs-exfat fusefs-ntfs fusefs-hfsfuse fusefs-lkl fusefs-simple-mtpfs
+pkg install -y wayland seatd vim zsh libreoffice doas fastfetch xorg firefox wifibox e2fsprogs xfsprogs exfat-utils fusefs-exfat fusefs-ntfs fusefs-hfsfuse fusefs-lkl fusefs-simple-mtpfs
 echo 'snd_driver_load="YES"' >> /etc/rc.conf
 beep
 pw groupmod video -m root
 pkg install -y kde sddm
 sysrc dbus_enable="YES"
+sysrc seatd_enable="YES"
 sysrc sddm_enable="YES"
-sysrc sddm_lang="en_US"
-
+sysrc sddm_lang="en_GB"
